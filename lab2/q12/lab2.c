7e8ebff32e7e7c0ee97d7afd380a96e52eb78a2b
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-05-04 21:25:45 +0200

    add lab2q12

--- lab2/q12/lab2.c
+++ lab2/q12/lab2.c
@@ -13,35 +13,39 @@ int main(void)
 
 ISR (when_a)
 {
+    ActivateTask(task1);
+}
+
+ISR (when_b) 
+{
+    // Here we supposedly don't need to terminate t1 because we shutdown
+    printf("Emergency Stop\r\n");
+    CancelAlarm(halfSec);
+    ShutdownOS(OSDEFAULTAPPMODE);
+}
+
+TASK (task1)
+{
+    static bool active = true;
+
+    if (active) {
+        printf("Waking up...\r\n");
+        SetRelAlarm(halfSec, 50, 50);
+    } else {
+        printf("Sleeping...\r\n");
+        CancelAlarm(halfSec);
+    }
+    active = !active;
+}
+
+TASK (task2)
+{
     static bool set = true;
 
     if (set) {
         set_leds(GREEN);
     } else {
         reset_leds(GREEN);
-    }
-    set = !set;
-}
-
-ISR (when_b)
-{
-    static bool set = true;
-    if (set) {
-        set_leds(BLUE);
-    } else {
-        reset_leds(BLUE);
-    }
-    set = !set;
-}
-
-TASK (task1)
-{
-    static bool set = true;
-
-    if (set) {
-        set_leds(RED);
-    } else {
-        reset_leds(RED);
     }
     set = !set;
 }
